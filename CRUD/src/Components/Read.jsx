af92d632831b384ea56ebf88e7e34b635dc5fe46
Giridharan72 <113990195+Giridharan72@users.noreply.github.com>
2024-02-06 12:13:04 +0530

    Update Read.jsx

--- CRUD/src/Components/Read.jsx
+++ CRUD/src/Components/Read.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Container, Table } from 'react-bootstrap';
+import { Container, Table, Spinner } from 'react-bootstrap';
 
 function Read() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchUsers();
@@ -13,8 +14,10 @@ function Read() {
     try {
       const response = await axios.get('https://mock-api-bdg1.onrender.com/data');
       setUsers(response.data);
+      setLoading(false);
     } catch (error) {
       console.error('Error fetching users:', error);
+      setLoading(false);
     }
   };
 
@@ -22,30 +25,37 @@ function Read() {
     <>
       <Container fluid>
         <h1>Users</h1>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Company</th>
-              <th>Website</th>
-              <th>Phone</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user => (
-              <tr key={user.id}>
-                <td>{user.id}</td>
-                <td>{user.name ? user.name : 'N/A'}</td>
-                <td>{user.email ? user.name : 'N/A'}</td>
-                <td>{user.company ? user.company.name : 'N/A'}</td>
-                <td>{user.website ? user.name : 'N/A'}</td>
-                <td>{user.phone ? user.name : 'N/A'}</td>  
+        {loading ? (
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        ) : (
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Company</th>
+                <th>Website</th>
+                <th>Phone</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody>
+            {users.map(user => (
+    <tr key={user.id}>
+        <td>{user.id}</td>
+        <td>{user.name ? user.name : 'N/A'}</td>
+        <td>{user.email ? user.email : 'N/A'}</td>
+        <td>{user.company?.name ? user.company.name : 'N/A'}</td> {/* Using optional chaining */}
+        <td>{user.website ? user.website : 'N/A'}</td>
+        <td>{user.phone ? user.phone : 'N/A'}</td>  
+    </tr>
+))}
+
+            </tbody>
+          </Table>
+        )}
       </Container>
     </>
   );
